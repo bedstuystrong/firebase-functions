ee6f153272f18c9bb6bd4f3f0a50e420163efed4
maybenotmaybe <maybenotmaybe@gmail.com>
2020-03-26 00:32:01 -0400

    Address comments

--- functions/airtableTriager.js
+++ functions/airtableTriager.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions');
 const { getChangedIntakeTickets } = require('./airtable');
 
 // Runs every minute
-exports.poll = functions.pubsub.schedule('* * * * *').onRun(async () => {
+exports.poll.airtableIntakeTickets = functions.pubsub.schedule('* * * * *').onRun(async () => {
     const changedTickets = await getChangedIntakeTickets()
 
     if (changedTickets.length === 0) {
