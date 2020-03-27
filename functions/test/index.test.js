76936c6614ade58ca77476fc996c10e929aaaa24
maybejustmaybe <60577309+maybejustmaybe@users.noreply.github.com>
2020-03-27 09:19:55 -0400

    Add a trigger for new intake records (#12)
    The trigger mechanism calls relevant callbacks everytime a ticket status changes. The intake trigger will post a message to the neighborhood channel every time a new ticket is created.
    
    Co-authored-by: maybenotmaybe <maybenotmaybe@gmail.com>

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,8 +1,9 @@
-const assert = require("assert")
 const test = require('firebase-functions-test')()
 
+const assert = require("assert")
 const mocha = require("mocha")
 
+const GARAGE_CHAN_ID = "C0106GP18UT"
 test.mockConfig(
     {
         "airtable": {
@@ -11,12 +12,22 @@ test.mockConfig(
             "base_id": "<REPLACE>",
             "intake_messages_table": "_test_sms_intake_messages",
             "inbound_table": "_test_inbound",
-            "intake_table": "_test_intake"
+            "intake_table": "_test_intake",
+        },
+        "slack": {
+            "token": "<REPLACE>",
+            "northeast_bedstuy": GARAGE_CHAN,
+            "northwest_bedstuy": GARAGE_CHAN,
+            "southeast_bedstuy": GARAGE_CHAN,
+            "southwest_bedstuy": GARAGE_CHAN,
         },
     }
 )
 
+const Slack = require("slack")
 const { getAllIntakeTickets, getChangedIntakeTickets } = require("../airtable")
+
+const bot = new Slack({ "token": "<REPLACE>" })
 
 
 describe("test get all intake tickets", () => {
@@ -30,5 +41,25 @@ describe("test get all new tickets", () => {
     it("basic", async () => {
         const tickets = await getChangedIntakeTickets()
         console.log(tickets.length)
+    })
+})
+
+describe("test slack", () => {
+    it("list channels", async () => {
+        const res = await bot.channels.list()
+        const channels = res.channels
+
+        for (const chan of channels) {
+            if (chan.name === "garage") {
+                return
+            }
+        }
+    })
+
+    it("send test message", async () => {
+        const res = await bot.chat.postMessage({
+            "channel": GARAGE_CHAN_ID,
+            "text": "HELLO!",
+        })
     })
 })
