36e05e4169ea422626f2d121cd00517b783b34c7
alex baldwin <ghostbaldwin@gmail.com>
2020-04-07 13:40:44 -0400

    move to donations-email function

---
+++ functions/email.js
@@ -0,0 +1,20 @@
+const Busboy = require('busboy');
+
+const sendgridMiddleware = (req, res, next) => {
+  const busboy = new Busboy({ headers: req.headers });
+
+  busboy.on('field', (fieldname, value) => {
+    req.body[fieldname] = value;
+  });
+
+  busboy.on('finish', () => {
+    next();
+  });
+
+  busboy.end(req.rawBody);
+};
+
+
+module.exports = {
+  sendgridMiddleware,
+};
