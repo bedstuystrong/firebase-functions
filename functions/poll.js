87ed305d9c0feb6c9c7f1ac10dc1d7a0ba0dd581
maybejustmaybe <maybenotmaybe@gmail.com>
2020-05-24 14:03:34 -0400

    Add another intake status

--- functions/poll.js
+++ functions/poll.js
@@ -486,6 +486,7 @@ module.exports = {
       'Assigned / In Progress': [onIntakeAssigned],
       'Complete': [onIntakeCompleted],
       'Not Bed-Stuy': [],
+      'Assistance no longer required': [],
     };
 
     return await pollTable(INTAKE_TABLE, STATUS_TO_CALLBACKS);
