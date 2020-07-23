7a73001e5518340ef91e134e594bad1ed004224b
Miriam Lauter <lauter.miriam@gmail.com>
2020-07-22 21:37:19 -0400

    Add some more functionality to devtools script

--- functions/scripts/devtools.js
+++ functions/scripts/devtools.js
@@ -3,7 +3,7 @@ const Slack = require('slack');
 const allSettled = require('promise.allsettled');
 const _ = require('lodash');
 const fs = require('fs');
-const {argv} = require('yargs');
+const yargs = require('yargs');
 
 allSettled.shim();
 
@@ -23,6 +23,7 @@ const {
   getVolunteerSlackID,
   storeMeta,
   updateRecord,
+  getVolunteerBySlackID,
 } = require('../airtable');
 
 const {
@@ -33,10 +34,13 @@ const {
   getIntakePostContent,
   getIntakePostDetails,
   getDeliveryDMContent,
+  renderDeliveryDM,
   getTicketSummaryBlocks,
   getShoppingList,
   renderShoppingList,
 } = require('../messages');
+
+const bot = new Slack({ token: functions.config().slack.token });
 
 async function getOneIntaketicket(ticket) {
   const records = await getRecordsWithTicketID(INTAKE_TABLE, ticket);
@@ -52,19 +56,48 @@ const saveIntakeTicketInfo = async (ticket) => {
 };
 
 const showShoppingList = async (tickets) => {
-  const records = await Promise.all(_.map(tickets.split(','), getOneIntaketicket));
+  const records = await Promise.all(_.map(tickets, getOneIntaketicket));
   const email = await getShoppingList(records);
   console.log(renderShoppingList(email));
 };
 
+const sendDeliveryDM = async (ticket) => {
+  const [, fields] = await getOneIntaketicket(ticket);
+  const deliveryDMContent = await getDeliveryDMContent(fields);
+  const renderedContent = renderDeliveryDM(fields.ticketID, deliveryDMContent, functions.config().slack.test_user_id);
+  await bot.chat.postMessage(renderedContent);
+  console.log(renderedContent);
+};
+
 const main = async () => {
-  if (argv.saveTicket !== undefined) {
-    await saveIntakeTicketInfo(argv.saveTicket);
-  } else if (argv.showShoppingList !== undefined) {
-    await showShoppingList(argv.showShoppingList);
-  } else {
-    console.error('no arguments matched TODO help text');
-  }
+  yargs
+    .usage('Usage: $0 <cmd> [options]')
+    .command({
+      command: 'save-ticket <ticketID>',
+      desc: 'Save an intake ticket record',
+      handler: async (argv) => {
+        await saveIntakeTicketInfo(argv.ticketID);
+      }
+    })
+    .command({
+      command: 'show-shopping-list [ticketIDs..]',
+      desc: 'Show the shopping list for a group of tickets',
+      handler: async (argv) => {
+        await showShoppingList(argv.ticketIDs);
+      }
+    })
+    .command({
+      command: 'send-delivery-dm <ticketID>',
+      desc: 'Send a test delivery DM for the given ticket to the slack.test_user_id',
+      handler: async (argv) => {
+        await sendDeliveryDM(argv.ticketID);
+      }
+    })
+    .help('help').alias('help', 'h')
+    .showHelpOnFail(true, 'whoops, something went wrong! run with --help')
+    .version(false)
+    .demandCommand(1)
+    .argv;
 };
 
 main().catch(e => { console.error(e); process.exit(1); });
