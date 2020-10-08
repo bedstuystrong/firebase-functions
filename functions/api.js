2a21242f9ee3fffb14bae6b8315d8e4fc2915bcd
alex baldwin <ghostbaldwin@gmail.com>
2020-10-08 00:20:34 -0400

    update api for twilio plugin, remove iframe version

--- functions/api.js
+++ functions/api.js
@@ -5,23 +5,25 @@ const _ = require('lodash');
 
 const {
   getRecordsWithPhoneNumber,
-  getRecord,
-  VOLUNTEER_FORM_TABLE,
+  // getRecord,
+  // VOLUNTEER_FORM_TABLE,
   INTAKE_TABLE,
 } = require('./airtable');
 
 allSettled.shim();
 
-const acceptableFields = ['ticketID', 'status', 'record ID', 'requestName', 'nearestIntersection', 'phoneNumber', 'dateCreated'];
+const acceptableFields = ['ticketID', 'status', 'recordID', 'requestName', 'nearestIntersection', 'phoneNumber', 'dateCreated', 'dateCompleted'];
 
 module.exports = {
 
   findTicketsByPhoneNumber: functions.https.onRequest(async (req, res) => {
-    if (!req.headers.referer) {
+    if (!req.headers.origin) {
       return res.status(401).send('Unauthorized');
     }
 
-    res.set('Access-Control-Allow-Origin', 'https://bedstuystrong-automation-a4b75.web.app');
+    // res.set('Access-Control-Allow-Origin', 'https://bedstuystrong-automation-a4b75.web.app');
+    // res.set('Access-Control-Allow-Origin', 'https://flex.twilio.com ');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     if (!req.query.phoneNumber) {
       res.json([]);
@@ -34,19 +36,34 @@ module.exports = {
     const phoneNumber = parsePhoneNumberFromString(req.query.phoneNumber).formatNational();
     const tickets = await getRecordsWithPhoneNumber(INTAKE_TABLE, phoneNumber);
 
-    const ticketsWithVolunteers = await Promise.allSettled(tickets.map(async ([, fields, ]) => {
-      const [, intakeVolunteer, ] = await getRecord(VOLUNTEER_FORM_TABLE, fields.intakeVolunteer);
-      const [, deliveryVolunteer, ] = await getRecord(VOLUNTEER_FORM_TABLE, fields.deliveryVolunteer);
-
+    const cleanTickets = tickets.map(([, fields, ]) => {
       const filteredFields = _.pick(fields, acceptableFields);
-
       return Object.assign(filteredFields, {
-        intakeVolunteer: intakeVolunteer.name,
-        deliveryVolunteer: deliveryVolunteer.name,
+        phoneNumber: req.query.phoneNumber,
       });
-    }));
+    });
 
-    res.json(ticketsWithVolunteers.map(result => result.value));
+    res.json(cleanTickets);
+
+    // const ticketsWithVolunteers = await Promise.allSettled(tickets.map(async ([, fields, ]) => {
+    //   const [, intakeVolunteer, ] = await getRecord(VOLUNTEER_FORM_TABLE, fields.intakeVolunteer);
+    //   const [, deliveryVolunteer, ] = await getRecord(VOLUNTEER_FORM_TABLE, fields.deliveryVolunteer);
+
+    //   const filteredFields = _.pick(fields, acceptableFields);
+
+    //   return Object.assign(filteredFields, {
+    //     phoneNumber: req.query.phoneNumber,
+    //     intakeVolunteer: intakeVolunteer.name,
+    //     deliveryVolunteer: deliveryVolunteer.name,
+    //   });
+    // }));
+
+    // TODO
+    // if (_.some(ticketsWithVolunteers, ['status', 'rejected'])) {
+    //    handle error
+    // }
+
+    // res.json(ticketsWithVolunteers.map(result => result.value));
   }),
 
 };
